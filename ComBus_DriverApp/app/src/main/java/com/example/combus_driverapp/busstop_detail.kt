f35288bea0129e7fc81104b1a915d3f97c4320e7
Sinyoung Kang <sinyoung6491@gmail.com>
2024-01-11 17:57:33 +0900

    second commit

---
+++ ComBus_DriverApp/app/src/main/java/com/example/combus_driverapp/busstop_detail.kt
@@ -0,0 +1,11 @@
+package com.example.combus_driverapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class busstop_detail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_busstop_detail)
+    }
+}
