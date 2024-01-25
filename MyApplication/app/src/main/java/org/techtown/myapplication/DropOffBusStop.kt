18562e36ef44e5da58f0733ea5621bb79c04b9e1
13b13 <20221175@sungshin.ac.kr>
2024-01-25 14:37:41 +0900

    last page & button

--- MyApplication/app/src/main/java/org/techtown/myapplication/DropOffBusStop.kt
+++ MyApplication/app/src/main/java/org/techtown/myapplication/DropOffBusStop.kt
@@ -1,6 +1,4 @@
 package org.techtown.myapplication
-
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -21,34 +19,37 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import org.techtown.myapplication.Retrofit.ApiManager_BoardingBusStop
+import org.techtown.myapplication.Retrofit.ApiManager_DropOffBusStop
+import org.techtown.myapplication.Retrofit.ApiManager_ReservationComplete
+import org.techtown.myapplication.Retrofit.ApiResponse
 import org.techtown.myapplication.Retrofit.BoardingStop
+import org.techtown.myapplication.Retrofit.DropOffStop
 import org.techtown.myapplication.Retrofit.LocationRequest
-import org.techtown.myapplication.databinding.ActivityBoardingBusStopBinding
+import org.techtown.myapplication.Retrofit.ReservationComplete
+import org.techtown.myapplication.databinding.ActivityDropOffBusStopBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class BoardingBusStop : AppCompatActivity(), OnMapReadyCallback {
+class DropOffBusStop : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var googleMap: GoogleMap
     private lateinit var mapView: MapView
+    private var selectedDropOffBusStop: DropOffStop? = null
+    private var busStops: List<DropOffStop>? = null
 
-    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityBoardingBusStopBinding.inflate(layoutInflater)
+        val binding = ActivityDropOffBusStopBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 위치 권한 체크
         if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            initializeMap() // 위치 권한이 있는 경우 지도 초기화
+            initializeMap()
         } else {
-            // 위치 권한이 없을 경우 권한을 요청
             requestLocationPermission()
         }
 
-        // EditText에 대한 이벤트 핸들러 추가
-        val searchBox = findViewById<EditText>(R.id.searchBox_boarding)
+        val searchBox = findViewById<EditText>(R.id.searchBox_drop_off)
         searchBox.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
                 // 텍스트가 변경된 후 호출되는 부분
@@ -64,9 +65,90 @@ class BoardingBusStop : AppCompatActivity(), OnMapReadyCallback {
                 searchBusStops(query)
             }
         })
+
+        val resultContainer = findViewById<LinearLayout>(R.id.resultContainer)
+
+        for (i in 0 until resultContainer.childCount) {
+            val cardView = resultContainer.getChildAt(i) as? CardView
+            cardView?.setOnClickListener {
+                val position = resultContainer.indexOfChild(cardView)
+                selectedDropOffBusStop = busStops?.get(position)
+                navigateToReservedPage()
+            }
+        }
     }
 
-    // 권한 요청 메서드
+    private fun navigateToDetailsActivity(busStop: DropOffStop) {
+        val intent = Intent(this, BusSelection::class.java)
+        intent.putExtra("busStop", busStop)
+        startActivity(intent)
+    }
+
+    private fun sendReservationToServer(reservation: ReservationComplete) {
+        val apiManager = ApiManager_ReservationComplete.create()
+        // Retrofit을 사용하여 서버 통신을 위한 인터페이스 생성
+
+        // 서버에 예약 정보 전송
+        apiManager.createReservation(reservation)
+            .enqueue(object : Callback<ApiResponse> {
+                override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
+                    if (response.isSuccessful) {
+                        // 서버 응답이 성공적으로 수신된 경우
+                        val apiResponse = response.body()
+
+                        // 여기에서 apiResponse를 활용하여 성공 또는 실패에 대한 처리를 수행
+                        if (apiResponse != null) {
+                            // 성공적으로 예약이 생성된 경우
+                            // apiResponse의 상세 정보에 따라 적절한 작업 수행
+                            if (apiResponse.status == "CREATED") {
+                                // 예약이 성공적으로 생성된 경우
+                                // 예약 성공 메시지를 사용자에게 보여줄 수 있습니다.
+                                showToast(apiResponse.detail)
+                            } else {
+                                // 예약이 실패한 경우
+                                // 실패 상세 정보에 따라 적절한 에러 메시지를 사용자에게 보여줄 수 있습니다.
+                                showToast(apiResponse.detail)
+                            }
+                        }
+                    } else {
+                        // 서버에서 오류 응답을 받음
+                        // response.code(), response.message() 등을 활용하여 처리
+                        showToast("서버 오류: ${response.code()}")
+                    }
+                }
+
+                override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
+                    // 서버에 예약 정보 전송 실패
+                    // 에러 메시지를 출력하거나 다른 예외 처리 작업을 수행
+                    showToast("예약 실패: ${t.message}")
+                }
+            })
+    }
+
+    // showToast 함수는 사용자에게 메시지를 보여주는 함수입니다.
+    fun showToast(message: String) {
+        // TODO: 사용자에게 메시지를 보여주는 로직을 여기에 추가
+    }
+
+
+    private fun navigateToReservedPage() {
+        // 서버로 예약 정보 전송하기
+        // 예약 정보 생성
+        val reservation = ReservationComplete(
+            boardingStop = "승차 정류소 고유 번호",  // TODO: 실제 데이터로 대체
+            dropStop = selectedDropOffBusStop?.arsId ?: "",  // 수정된 부분
+            vehId = "버스 ID",  // TODO: 실제 데이터로 대체
+            busRouteNm = "버스 노선 번호"  // TODO: 실제 데이터로 대체
+        )
+
+        // 서버에 예약 정보 전송
+        sendReservationToServer(reservation)
+
+        val intent = Intent(this, Reserved::class.java)
+        intent.putExtra("dropOffBusStop", selectedDropOffBusStop)
+        startActivity(intent)
+    }
+
     private fun requestLocationPermission() {
         ActivityCompat.requestPermissions(
             this,
@@ -75,7 +157,6 @@ class BoardingBusStop : AppCompatActivity(), OnMapReadyCallback {
         )
     }
 
-    // 권한 요청 결과 처리
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -84,7 +165,7 @@ class BoardingBusStop : AppCompatActivity(), OnMapReadyCallback {
         when (requestCode) {
             REQUEST_LOCATION_PERMISSION -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    initializeMap()  // 권한 허용된 경우 지도 초기화
+                    initializeMap()
                 } else {
                     // 권한이 거부된 경우 처리
                     // 사용자에게 권한이 필요하다는 안내 메시지를 보여주거나 다른 처리를 수행
@@ -96,28 +177,27 @@ class BoardingBusStop : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    companion object {
-        private const val REQUEST_LOCATION_PERMISSION = 1001
-    }
-
     private fun initializeMap() {
-        // 위치 권한 확인
         if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // 예약 정보 가져오기
             val locationRequest = LocationRequest(126.9723, 37.5562)
-            val busStopService = ApiManager_BoardingBusStop.create()
+            val busStopService = ApiManager_DropOffBusStop.create()
 
-            busStopService.getNearbyBusStops(locationRequest)
-                .enqueue(object : Callback<List<BoardingStop>> {
-                    override fun onResponse(call: Call<List<BoardingStop>>, response: Response<List<BoardingStop>>) {
+            // 하차 정류소 정보 가져오기
+            busStopService.getDropOffBusStops()
+                .enqueue(object : Callback<List<DropOffStop>> {
+                    override fun onResponse(
+                        call: Call<List<DropOffStop>>,
+                        response: Response<List<DropOffStop>>
+                    ) {
                         if (response.isSuccessful) {
-                            val busStops = response.body()
-                            // 여기에서 busStops를 활용하여 UI 업데이트 또는 다른 작업 수행
+                            busStops = response.body()
 
                             // 구글 지도에 버스 정류장 마커 표시
                             for (busStop in busStops.orEmpty()) {
                                 val busStopLatLng = LatLng(busStop.latitude, busStop.longitude)
-                                googleMap.addMarker(MarkerOptions().position(busStopLatLng).title(busStop.name))
+                                googleMap.addMarker(
+                                    MarkerOptions().position(busStopLatLng).title(busStop.name)
+                                )
                             }
                         } else {
                             // API 호출은 성공했지만 서버에서 오류 응답을 받은 경우
@@ -125,18 +205,16 @@ class BoardingBusStop : AppCompatActivity(), OnMapReadyCallback {
                         }
                     }
 
-                    override fun onFailure(call: Call<List<BoardingStop>>, t: Throwable) {
+                    override fun onFailure(call: Call<List<DropOffStop>>, t: Throwable) {
                         // API 호출에 실패한 경우
                         // 에러 메시지를 출력하거나 다른 예외 처리 작업을 수행
                     }
                 })
 
-            // 맵뷰 초기화
             mapView = findViewById(R.id.mapView)
-            mapView.onCreate(null) // savedInstanceState 대신 null을 전달
+            mapView.onCreate(null)
             mapView.getMapAsync(this)
         } else {
-            // 위치 권한이 없을 경우 권한을 요청
             requestLocationPermission()
         }
     }
@@ -144,9 +222,7 @@ class BoardingBusStop : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(gMap: GoogleMap) {
         googleMap = gMap
 
-        // 위치 권한 확인
         if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // 위치 정보 가져오기 및 지도에 표시
             val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
             val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
 
@@ -156,7 +232,6 @@ class BoardingBusStop : AppCompatActivity(), OnMapReadyCallback {
                 googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
             }
         } else {
-            // 위치 권한이 없을 경우 권한을 요청
             requestLocationPermission()
         }
     }
@@ -188,11 +263,14 @@ class BoardingBusStop : AppCompatActivity(), OnMapReadyCallback {
 
     private fun searchBusStops(query: String) {
         val locationRequest = LocationRequest(126.9723, 37.5562)
-        val busStopService = ApiManager_BoardingBusStop.create()
+        val busStopService = ApiManager_DropOffBusStop.create()
 
-        busStopService.getNearbyBusStops(locationRequest)
-            .enqueue(object : Callback<List<BoardingStop>> {
-                override fun onResponse(call: Call<List<BoardingStop>>, response: Response<List<BoardingStop>>) {
+        busStopService.getDropOffBusStops() // 수정된 부분
+            .enqueue(object : Callback<List<DropOffStop>> {
+                override fun onResponse(
+                    call: Call<List<DropOffStop>>,
+                    response: Response<List<DropOffStop>>
+                ) {
                     if (response.isSuccessful) {
                         val busStops = response.body()
                         val filteredBusStops = busStops?.filter { it.name.contains(query, ignoreCase = true) }
@@ -204,14 +282,14 @@ class BoardingBusStop : AppCompatActivity(), OnMapReadyCallback {
                     }
                 }
 
-                override fun onFailure(call: Call<List<BoardingStop>>, t: Throwable) {
+                override fun onFailure(call: Call<List<DropOffStop>>, t: Throwable) {
                     // API 호출에 실패한 경우
                     // 에러 메시지를 출력하거나 다른 예외 처리 작업을 수행
                 }
             })
     }
 
-    private fun updateUIWithSearchResults(busStops: List<BoardingStop>?) {
+    private fun updateUIWithSearchResults(busStops: List<DropOffStop>?) {
         val resultContainer = findViewById<LinearLayout>(R.id.resultContainer)
 
         // 기존의 뷰들을 모두 제거
@@ -225,17 +303,15 @@ class BoardingBusStop : AppCompatActivity(), OnMapReadyCallback {
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT
             )
-            layoutParams.topMargin = 8 // 간격 조절
+            layoutParams.topMargin = 8
             cardView.layoutParams = layoutParams
-            cardView.radius = 16f // 둥근 모서리 반경 설정
-            cardView.useCompatPadding = true // 내용물과 패딩을 호환성 있게 사용
+            cardView.radius = 16f
+            cardView.useCompatPadding = true
 
             // TextView 추가
             val textView = TextView(this)
             textView.text = busStop.name
             textView.setOnClickListener {
-                // 여기에 해당 승차 정류장을 클릭했을 때의 동작을 추가
-                // 예를 들어, 클릭한 정류장의 위치로 지도를 이동하거나 해당 정류장의 상세 정보를 표시하는 등의 동작을 추가할 수 있음
                 navigateToDetailsActivity(busStop)
             }
 
@@ -246,10 +322,8 @@ class BoardingBusStop : AppCompatActivity(), OnMapReadyCallback {
             resultContainer.addView(cardView)
         }
     }
-    // 클릭한 정류장의 정보를 다른 액티비티로 전달하면서 액티비티 전환
-    private fun navigateToDetailsActivity(busStop: BoardingStop) {
-        val intent = Intent(this, BusSelection::class.java)
-        intent.putExtra("busStop", busStop)
-        startActivity(intent)
+
+    companion object {
+        private const val REQUEST_LOCATION_PERMISSION = 1001
     }
 }
--- MyApplication/app/src/main/java/org/techtown/myapplication/Retrofit/Home_Reservation.kt
+++ MyApplication/app/src/main/java/org/techtown/myapplication/Retrofit/Home_Reservation.kt
@@ -7,6 +7,8 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 data class HomeReservationResponse(
     @SerializedName("timestamp") val timestamp: String,
@@ -63,11 +65,14 @@ data class ReservationData(
 interface HomeReservationService {
     @GET("users/home")
     fun getHomeReservation(): Call<HomeReservationResponse>
+
+    @PUT("update-reservation-status")
+    fun updateReservationStatus(@Query("reservationId") reservationId: Long?, @Query("newStatus") newStatus: String): Call<HomeReservationResponse>
 }
 
 class ApiManager_homeReservation {
     companion object {
-        private const val BASE_URL = "<https://your-api-base-url.com/>" // 실제 API의 베이스 URL로 변경
+        private const val BASE_URL = "<https://your-api-base-url.com/>"
 
         fun create(): HomeReservationService {
             val retrofit = Retrofit.Builder()
@@ -77,5 +82,17 @@ class ApiManager_homeReservation {
 
             return retrofit.create(HomeReservationService::class.java)
         }
+
+        // 예약 상태 업데이트 함수 추가
+        fun updateReservationStatus(reservationId: Long?, newStatus: String): Call<HomeReservationResponse> {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            val service = retrofit.create(HomeReservationService::class.java)
+            return service.updateReservationStatus(reservationId, newStatus)
+        }
     }
 }
+
