b8caff68842303e875e4aca5540d052143df2456
Sinyoung Kang <sinyoung6491@gmail.com>
2024-02-02 16:39:05 +0900

    api_1

--- ComBus_DriverApp/app/src/main/java/com/example/combus_driverapp/busstop_list_adapter.kt
+++ ComBus_DriverApp/app/src/main/java/com/example/combus_driverapp/busstop_list_adapter.kt
@@ -1,39 +1,58 @@
 package com.example.combus_driverapp
 
+import android.app.Dialog
+import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.combus_driverapp.connection.RetrofitClient
 import com.example.combus_driverapp.databinding.HomeListItemBinding
 import timber.log.Timber
 
-class busstop_list_adapter(private val busstoplist:List<busstop>,/*bookpersonlist:List<busstop_book_person>,alightpersonlist:List<busstop_alight_person>*/):RecyclerView.Adapter<busstop_list_adapter.busstop_list_ViewHolder>() {
+class busstop_list_adapter(private val busstoplist: List<RetrofitClient.homebusStopList>,private val busPos:List<RetrofitClient.homebusPos>):RecyclerView.Adapter<busstop_list_adapter.busstop_list_ViewHolder>() {
 
-    val book = 0
-    val alight = 0
-    val chair = false
-
-    fun count(busstoplist: List<busstop>,bookpersonlist: List<busstop_book_person>,alightpersonlist: List<busstop_alight_person>){
-        for (i in 1..bookpersonlist.size){
-
-        }
-    }
-
+    //private val dialog = Dialog(context:Context)
     class busstop_list_ViewHolder(val binding: HomeListItemBinding):
             RecyclerView.ViewHolder(binding.root){
         private val context = binding.root.context
-                fun bind(busstop:busstop){
-                    binding.txtBusstopNameItem.text = busstop.busstop_name
-                    binding.txtBusstopNumItem.text = busstop.busstop_num
+                fun bind(busstop:RetrofitClient.homebusStopList,busPos:RetrofitClient.homebusPos){
+                    binding.txtBusstopNameItem.text = busstop.name
+                    binding.txtBusstopNumItem.text = busstop.arsId.toString()
+                    if (busstop.wheelchair == true)
+                        binding.wheelchairIcon.visibility = View.VISIBLE
+                    else binding.wheelchairIcon.visibility = View.GONE
+                    if (busPos.arsId == busstop.arsId){
+                        if (busPos.stopFlag == true)
+                            binding.imageView7.setImageResource(R.drawable.home_bus)
+                        //else binding.imageView7.setImageResource(R.drawable.home_busstop_base)
+                    }
+                    else binding.imageView7.setImageResource(R.drawable.home_busstop_base)
+                    if (busstop.reserved_cnt>0) {
+                        binding.txtBusstopBookNumItem.visibility = View.VISIBLE
+                        binding.txtBusstopBookNumItem.text = "예약 ${busstop.reserved_cnt}"
+                    }
+                    else binding.txtBusstopBookNumItem.visibility = View.GONE
+                    if (busstop.drop_cnt>0) {
+                        binding.txtBusstopOutNumItem.visibility = View.VISIBLE
+                        binding.txtBusstopOutNumItem.text = "하차 ${busstop.drop_cnt}"
+                    }
+                    else binding.txtBusstopBookNumItem.visibility = View.GONE
+
+
 
                     itemView.setOnClickListener {
                         val intent = Intent(context,busstop_detail::class.java)
-                        intent.putExtra("busstop_name",busstop.busstop_name)
-                        intent.putExtra("busstop_num",busstop.busstop_num)
+                        intent.putExtra("busstop_name",busstop.name)
+                        intent.putExtra("busstop_num",busstop.arsId)
                         intent.run { context.startActivity(this) }
                     }
                 }
             }
+    /*fun MyDig(){
+        dialog
+    }*/
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): busstop_list_ViewHolder {
         Timber.d("onCreateViewHolder")
@@ -46,8 +65,9 @@ class busstop_list_adapter(private val busstoplist:List<busstop>,/*bookpersonlis
 
     override fun onBindViewHolder(holder: busstop_list_ViewHolder, position: Int) {
         Timber.d("onBindViewHolder")
-        val currentMaster = busstoplist[position]
-        holder.bind(currentMaster)
+        val currentMaster1 = busstoplist[position]
+        val currentMaster2 = busPos[position]
+        holder.bind(currentMaster1,currentMaster2)
     }
 
 }
--- ComBus_DriverApp/app/src/main/java/com/example/combus_driverapp/connection/RetrofitClient.kt
+++ ComBus_DriverApp/app/src/main/java/com/example/combus_driverapp/connection/RetrofitClient.kt
@@ -29,4 +29,60 @@ class RetrofitClient {
         @SerializedName("loginId")
         val loginIn:String
     )
+    data class requesthome(
+        @SerializedName("driverId")
+        val driverId:Long
+    )
+    data class responsehome(
+        @SerializedName("timestamp")
+        val timestamp:String,
+        @SerializedName("code")
+        val code:String,
+        @SerializedName("status")
+        val status:String,
+        @SerializedName("detail")
+        val detail:String,
+        @SerializedName("data")
+        val data:homedata
+    )
+    data class homedata(
+        @SerializedName("vehId")
+        val vehId:Long,
+        @SerializedName("busRouteName")
+        val busRouteName:String,
+        @SerializedName("totalReserved")
+        val totalReserved:Int,
+        @SerializedName("totalDrop")
+        val totalDrop:Int,
+        @SerializedName("busPos")
+        val busPos:homebusPos,
+        @SerializedName("busStopList")
+        val busStopList:homebusStopList
+    )
+    data class homebusPos(
+        @SerializedName("arsId")
+        val arsId:Long,
+        @SerializedName("stSeq")
+        val stSeq:Int,
+        @SerializedName("stopFlag")
+        val stopFlag:Boolean
+    )
+    data class homebusStopList(
+        @SerializedName("arsId")
+        val arsId: Long,
+        @SerializedName("name")
+        val name:String,
+        @SerializedName("gpsX")
+        val gpsX:Double,
+        @SerializedName("gpsY")
+        val gpsY:Double,
+        @SerializedName("seq")
+        val seq:Int,
+        @SerializedName("reserved_cnt")
+        val reserved_cnt:Int,
+        @SerializedName("drop_cnt")
+        val drop_cnt:Int,
+        @SerializedName("wheelchair")
+        val wheelchair:Boolean
+    )
 }
