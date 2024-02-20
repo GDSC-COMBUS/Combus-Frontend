9ca6b0d7e2a7ce66168cbdf6417c8bf5a195f282
Sinyoung Kang <sinyoung6491@gmail.com>
2024-02-21 02:54:55 +0900

    take_photo

--- MyApplication/app/src/main/java/org/techtown/myapplication/Camera_page.kt
+++ MyApplication/app/src/main/java/org/techtown/myapplication/Camera_page.kt
@@ -49,7 +49,7 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 
 typealias LumaListener = (luma: Double) -> Unit
-class Camera_page : AppCompatActivity() {
+class Camera_page : AppCompatActivity(), TextToSpeech.OnInitListener {
     private lateinit var binding: ActivityCameraPageBinding
 
     private var imageCapture: ImageCapture? = null
@@ -89,15 +89,16 @@ class Camera_page : AppCompatActivity() {
 
         // Set up the listeners for take photo and video capture buttons
         binding.videoCaptureButton.setOnClickListener {
-            binding.busNumTxt.visibility = View.GONE
-            captureVideo()
+            //binding.busNumTxt.visibility = View.GONE
+            //captureVideo()
             //openAlbum()
+            takePhoto()
             }
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
     // TextToSpeech 초기화 완료 시 호출되는 콜백
-    fun onInit(status: Int) {
+    override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
             // TextToSpeech 초기화 성공 시 설정
             val locale = Locale("en", "US") // 영어 설정
@@ -199,6 +200,10 @@ class Camera_page : AppCompatActivity() {
                 .also {
                     it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
                 }
+            //
+            imageCapture = ImageCapture.Builder().build()
+            //
+
             val recorder = Recorder.Builder()
                 .setQualitySelector(QualitySelector.from(Quality.HIGHEST))
                 .build()
@@ -210,8 +215,13 @@ class Camera_page : AppCompatActivity() {
                 // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
 
+                //
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture)
+                //
+
                 // Bind use cases to camera
-                cameraProvider.bindToLifecycle(this, cameraSelector, preview, videoCapture)
+                //cameraProvider.bindToLifecycle(this, cameraSelector, preview, videoCapture)
 
             } catch(exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
@@ -399,4 +409,46 @@ class Camera_page : AppCompatActivity() {
                 }
             })
     }}
+
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time stamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+            .build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun
+                        onImageSaved(output: ImageCapture.OutputFileResults){
+                    val msg = "Photo capture succeeded: ${output.savedUri}"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            }
+        )
+    }
 }
